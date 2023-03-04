230bda3c1975dcd532c3b2053f51c51949787684
Selim Yaman <mselimyaman@gmail.com>
2023-03-04 14:25:46 +0300

    ufak düzeltmeler

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var value2 = "";
 var val1 = [];
 var val2 = [];
 var result = [];
+var tempResult = [];
+var tempResult2 = [];
 var isLess;
 var isEqual;
 var isZero;
@@ -21,6 +23,14 @@ function handleClick(x) {
   } else if (x === "multiply") {
     multiply(val1, val2);
   }
+  document.getElementById("sonuc").innerHTML = result.join("");
+
+  isZero = false;
+  isEqual = false;
+  isLess = false;
+  val1 = [];
+  val2 = [];
+  result = [];
 }
 
 function arrange(input1, input2) {
@@ -48,58 +58,11 @@ function arrange(input1, input2) {
       val2.unshift(input2[i]);
     }
 
-    for (var i = 0; i < val1.length; i++) {
-      switch (val1[i]) {
-        case "a":
-          val1.splice(i, 1, "10");
-          break;
-        case "b":
-          val1.splice(i, 1, "11");
-          break;
-        case "c":
-          val1.splice(i, 1, "12");
-          break;
-        case "d":
-          val1.splice(i, 1, "13");
-          break;
-        case "e":
-          val1.splice(i, 1, "14");
-          break;
-        case "f":
-          val1.splice(i, 1, "15");
-          break;
-      }
-    }
+    turnToDec(val1);
+    turnToDec(val2);
 
-    for (var i = 0; i < val2.length; i++) {
-      switch (val2[i]) {
-        case "a":
-          val2.splice(i, 1, "10");
-          break;
-        case "b":
-          val2.splice(i, 1, "11");
-          break;
-        case "c":
-          val2.splice(i, 1, "12");
-          break;
-        case "d":
-          val2.splice(i, 1, "13");
-          break;
-        case "e":
-          val2.splice(i, 1, "14");
-          break;
-        case "f":
-          val2.splice(i, 1, "15");
-          break;
-      }
-    }
-
-    for (var i = 0; i < val1.length; i++) {
-      val1.splice(i, 1, parseInt(val1[i]));
-    }
-    for (var i = 0; i < val2.length; i++) {
-      val2.splice(i, 1, parseInt(val2[i]));
-    }
+    turnToInt(val1);
+    turnToInt(val2);
 
     var diff = val1.length - val2.length;
     return val1, val2;
@@ -120,28 +83,59 @@ function checkEqual(input1, input2) {
   }
 }
 
-function turnToHex() {
-  for (var i = 0; i < result.length; i++) {
-    switch (result[i]) {
+function turnToHex(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
       case 10:
-        result.splice(i, 1, "a");
+        arr.splice(i, 1, "a");
         break;
       case 11:
-        result.splice(i, 1, "b");
+        arr.splice(i, 1, "b");
         break;
       case 12:
-        result.splice(i, 1, "c");
+        arr.splice(i, 1, "c");
         break;
       case 13:
-        result.splice(i, 1, "d");
+        arr.splice(i, 1, "d");
         break;
       case 14:
-        result.splice(i, 1, "e");
+        arr.splice(i, 1, "e");
         break;
       case 15:
-        result.splice(i, 1, "f");
+        arr.splice(i, 1, "f");
         break;
     }
+  }
+}
+
+function turnToDec(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "a":
+        arr.splice(i, 1, "10");
+        break;
+      case "b":
+        arr.splice(i, 1, "11");
+        break;
+      case "c":
+        arr.splice(i, 1, "12");
+        break;
+      case "d":
+        arr.splice(i, 1, "13");
+        break;
+      case "e":
+        arr.splice(i, 1, "14");
+        break;
+      case "f":
+        arr.splice(i, 1, "15");
+        break;
+    }
+  }
+}
+
+function turnToInt(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr.splice(i, 1, parseInt(arr[i]));
   }
 }
 
@@ -169,19 +163,12 @@ function add(input1, input2) {
   }
   result.reverse();
 
-  turnToHex();
-
-  document.getElementById("sonuc").innerHTML = result.join("");
-
-  val1 = [];
-  val2 = [];
-  result = [];
+  turnToHex(result);
 }
 
 function subtract(input1, input2) {
   if (isZero) {
     result = [0];
-    document.getElementById("sonuc").innerHTML = result.join("");
   } else {
     if (isEqual) {
       checkEqual(input1.reverse(), input2.reverse());
@@ -194,7 +181,6 @@ function subtract(input1, input2) {
     }
     input1.reverse();
     input2.reverse();
-    
 
     for (var i = 0; i < input2.length; i++) {
       var minus = input1[i] - input2[i];
@@ -206,9 +192,6 @@ function subtract(input1, input2) {
         result.push(input1[i]);
       }
     }
-    console.log(input1);
-    console.log(input2);
-    console.log(result);
 
     for (var i = 0; i < result.length; i++) {
       if (result[i] < 0) {
@@ -218,26 +201,14 @@ function subtract(input1, input2) {
       }
     }
     result.reverse();
-    turnToHex();
-
+    turnToHex(result);
     if (isLess) {
-      document.getElementById("sonuc").innerHTML = "-" + result.join("");
-    } else {
-      document.getElementById("sonuc").innerHTML = result.join("");
+      result.unshift("-");
     }
   }
-
-  isZero = false;
-  isEqual = false;
-  isLess = false;
-  val1 = [];
-  val2 = [];
-  result = [];
 }
 
 function multiply(input1, input2) {
-  var tempResult = [];
-  var tempResult2 = [];
   if (input2.length === 1) {
     for (var i = 0; i < input1.length; i++) {
       var temp = input1[i] * input2[0];
@@ -347,11 +318,42 @@ function multiply(input1, input2) {
     result = tempResult2.reverse();
   }
 
-  turnToHex();
+  turnToHex(result);
+}
+
+function divide(input1, input2) {
+  if (input1[0] === "-" || input2[0] === "-") {
+    alert("Please enter a positive number!");
+  } else {
+    for (var i = 0; i < input1.length; i++) {
+      val1.push(input1[i]);
+    }
+
+    for (var i = 0; i < input2.length; i++) {
+      val2.push(input2[i]);
+    }
+  }
+  turnToDec(val1);
+  turnToDec(val2);
+  turnToInt(val1);
+  turnToInt(val2);
+
+
+  if (input1 === input2) {
+    result = [1];
+  } else {
+    if (input1.length < input2.length) {
+    } else if (input1.length === input2.length) {
+    } else {
+      var div = val2.length;
+      checkEqual(val1.slice(0, div), val2);
+      if (isLess) {
+        div++;
+      } else {
+
+      }
+    }
+  }
 
   document.getElementById("sonuc").innerHTML = result.join("");
-
-  val1 = [];
-  val2 = [];
-  result = [];
 }
